5be0332b818acd70b0bf025d72f210113a6c0b70
joaopaulofg <joao.gomes@ccc.ufcg.edu.br>
2024-07-25 18:10:06 -0300

    Initial commit

---
+++ src/main/java/me/dio/SantanderBootcampJavaApplication.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampJavaApplication.class, args);
+	}
+
+}
